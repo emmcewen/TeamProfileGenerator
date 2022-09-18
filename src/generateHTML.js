41b23173a338a262d5682db8feaf8cc877bc16aa
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-18 12:43:24 -0400

    adding more generate HTML to feed into cards on page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,9 +1,9 @@
-function generateHTML(){
+function generateHTML() {
 
 }
 
-const generateManager = function(manager){
-    return`
+const generateManager = function (manager) {
+    return `
     <div class="card h-100">
         <div class="card-header">
             <h3 class="name">${manager.name}</h3>
@@ -17,8 +17,8 @@ const generateManager = function(manager){
         </div>
         `;
 }
-const generateEngineer = function(engineer){
-    return`
+const generateEngineer = function (engineer) {
+    return `
     <div class="card h-100">
         <div class="card-header">
             <h3 class="name">${engineer.name}</h3>
@@ -33,8 +33,8 @@ const generateEngineer = function(engineer){
 
 };
 
-const generateIntern= function(intern){
-    return`
+const generateIntern = function (intern) {
+    return `
     <div class="card h-100">
         <div class="card-header">
             <h3 class="name">${intern.name}</h3>
@@ -50,27 +50,59 @@ const generateIntern= function(intern){
         `;
 };
 
-generateHTML =(data)=> {
+generateHTML = (data) => {
     pageArray = [];
-    
-    for (let i = 0; i< data.length; i++) {
-    const employee = data[i];
-    const role = employee.getRole();
 
-    if (role ==="Manager"){
-        const managerCard = generateManager(employee);
-        pageArray.push (managerCard);
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
 
-    if (role ==="Engineer"){
-        const engineerCard = generateEngineer(employee);
-        pageArray.push (engineerCard);
+        if (role === "Manager") {
+            const managerCard = generateManager(employee);
+            pageArray.push(managerCard);
+
+            if (role === "Engineer") {
+                const engineerCard = generateEngineer(employee);
+                pageArray.push(engineerCard);
+            }
+            if (role === "Intern") {
+                const internCard = generateIntern(employee);
+                pageArray.push(internCard);
+
+            }
+        }
     }
-    if (role ==="Intern"){
-        const internCard = generateIntern(employee);
-        pageArray.push (internCard);
-    
 }
-    }
-}}
+const generateTeamProfile = function (employeeCards){
+    return`
+    
+    < !DOCTYPE html >
+    <html lang="en">
 
-module.exports=generateHTML;
+        <head>
+            <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Team Profile Generator</title>
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+                            <link rel="stylesheet" href="style.css" />
+
+                            <body>
+                                <header>
+                                    <nav class="navbar" id="navbar">
+                                        <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile Generator</span>
+                                    </nav>
+                                </header>
+                                <main>
+                                    <div class="container">
+                                        <div class="row justify-content-center" id="employee-cards">
+                                            ${employeeCards}
+                                        </div>
+                                    </div>
+                                </main>
+                            </body>
+                        </link>
+                        `;
+                        }
+                        
+                        module.exports=generateHTML;
